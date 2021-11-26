4be2ad6bf49118cb7b7609a4d7bc3625f3338b6e
Mataboy12k <augustohcoco@hotmail.com>
2021-11-26 00:38:11 +0000

    commit

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-tarefas = [
-    {"texto": "Estudar para a prova", "concluida": False},
-    {"texto": "Passear com o cachorro", "concluida": True},
-    {"texto": "Estudar mais para a prova", "concluida": False},
+bebidas = [
+    {"texto": "Coca-cola ", "concluida": ' R$ 4,10'},
+    {"texto": "Roller ", "concluida": ' R$ 2,49'},
+    {"texto": "Sprite ", "concluida": ' R$ 2,40'},
 ]
 
-@app.route('/')
+@app.route('/') 
 def index():
-    return render_template('index.html', lista=tarefas)
+    return render_template('index.html', lista=bebidas)
+
 
 @app.route('/create')
 def create():
@@ -19,13 +20,38 @@ def create():
 @app.route('/save', methods=['POST'])  # <form action="/save" method="POST">
 def save():
     texto = request.form['texto']      # <input name="texto"/>
-    tarefa = { "texto": texto, "concluida": False }
-    tarefas.append(tarefa)
+    preco = request.form['preco']
+    bebi = { "texto": texto, "concluida": preco }
+    bebidas.append(bebi)
 
-    return redirect('https://5000-brown-lemur-kiu2j61j.ws-us18.gitpod.io/')
+    return redirect('https://5000-red-rattlesnake-a9wqju4e.ws-us17.gitpod.io')
+
+@app.route('/busca', methods=['POST'])  
+def busca():
+    texto = request.form['pesquisa']
+    texto = texto.lower()      
+    lista = list()
+    for bebi in bebidas:
+        if texto in bebi['texto'].lower():
+            lista.append(bebi)
+    if not texto or not lista:
+      return render_template('erro.html')
+
+    return render_template('busca.html', lista2=lista)
+
+@app.route('/delete', methods=['POST'])  
+def delete():
+    deletar_item = request.form['deletar']
+    for bebi in bebidas:
+        if deletar_item == bebi['texto']:
+            bebidas.remove(bebi)
+    #if not deletar_item:
+      #return render_template('erro.html')
+
+    return render_template('index.html', lista=bebidas)
+
+@app.route('/criar')  
+def criar():
+    return render_template('delete.html')
 
 app.run(debug=True)
-
-
-# Implementar o DELETE!! (2,0 pontos)
-# Implementar uma pesquisa (3,0 pontos)
